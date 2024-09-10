5a090eb7ea179bdd3d26add72073e000f4f4a6a5
lianneg19 <125237077+lianneg19@users.noreply.github.com>
2024-09-10 00:26:00 -0500

    added controls

--- app/src/main/java/com/example/traditionalandroiddev/MainActivity.kt
+++ app/src/main/java/com/example/traditionalandroiddev/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.traditionalandroiddev
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.setPadding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val et_name: TextView = findViewById(R.id.et_name)
+        et_name.text = "Hi, My name is Lianne"
+        et_name.textSize = 20f
+        et_name.setPadding(20,20,20,20)
+        et_name.setBackgroundColor(resources.getColor(R.color.pink))
     }
 }
