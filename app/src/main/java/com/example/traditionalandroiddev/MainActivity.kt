4d0233714a92aee46bf8999014c5f183d9c60399
lianneg19 <125237077+lianneg19@users.noreply.github.com>
2024-09-10 09:27:36 -0500

    created 2nd page

--- app/src/main/java/com/example/traditionalandroiddev/MainActivity.kt
+++ app/src/main/java/com/example/traditionalandroiddev/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.traditionalandroiddev
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.setPadding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +20,19 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         val et_name: TextView = findViewById(R.id.et_name)
+        val btn_submit: Button = findViewById(R.id.btn_submit)
+
         et_name.text = "Hi, My name is Lianne"
         et_name.textSize = 20f
         et_name.setPadding(20,20,20,20)
         et_name.setBackgroundColor(resources.getColor(R.color.pink))
+
+        btn_submit.text = "Submit"
+        btn_submit.textSize = 20f
+        btn_submit.setPadding(20,20,20,20)
+
+        btn_submit.setOnClickListener {
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
